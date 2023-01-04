8b37add47222c279f738aff3680f69985f91c4af
Zane <zeisen33@gmail.com>
2023-01-04 15:23:45 -0500

    Two Sum

--- phase_1_arrays.js
+++ phase_1_arrays.js
@@ -7,3 +7,15 @@ Array.prototype.uniq = function() {
     }
     return newArray;
 }
+
+Array.prototype.twoSum = function() {
+    let newArray = [];
+    for (let i = 0; i < this.length; i++) {
+        for (let j = 0; j < this.length; j++) {
+            if (this[i] + this[j] === 0 && i < j) {
+                newArray.push([i, j]);
+            }
+        }
+    }
+    return newArray
+}
